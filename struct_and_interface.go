bf2b9591078181f02d15211b8f1d7b7eaa1181a8
wwyiwzhang <wwzhang@ip-192-168-1-102.us-west-2.compute.internal>
2017-07-22 14:07:51 -0700

    add interface

--- struct_and_interface.go
+++ struct_and_interface.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	tc1 := TempConvertor{18, "cel"}
@@ -9,6 +12,12 @@ func main() {
 	fmt.Println("celcius is", tc2.convertor())
 	tc3 := TempConvertor{100, "fap"}
 	fmt.Println("celcius is", tc3.convertor())
+	cir := Circle{1.5}
+	squ := Square{2}
+	fmt.Println("The area of a circle is", cir.getarea())
+	fmt.Println("The area of a square is", squ.getarea())
+	fmt.Println("The area of the object is", printArea(cir))
+	fmt.Println("The area of the object is", printArea(squ))
 }
 
 type TempConvertor struct {
@@ -16,6 +25,8 @@ type TempConvertor struct {
 	y string
 }
 
+// (tc *TempConvertor) is a function receiver
+// panic not recommended to use
 func (tc *TempConvertor) convertor() float64 {
 	defer func() {
 		r := recover()
@@ -31,4 +42,30 @@ func (tc *TempConvertor) convertor() float64 {
 	} else {
 		panic("unknown type!")
 	}
+}
+
+// interface
+// a very classic example
+type Circle struct {
+	radius float64
+}
+
+type Square struct {
+	side float64
+}
+
+func (c Circle) getarea() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (s Square) getarea() float64 {
+	return s.side * s.side
+}
+
+type Geometry interface {
+	getarea() float64
+}
+
+func printArea(g Geometry) float64 {
+	return g.getarea()
 }
