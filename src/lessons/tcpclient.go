67a4a6b98b2e1fe2344fc2519640b56990708a62
wwyiwzhang <wwzhang@ip-192-168-1-103.us-west-2.compute.internal>
2017-12-24 17:03:13 -0800

    clean up

--- src/lessons/tcpclient.go
+++ src/lessons/tcpclient.go
@@ -16,10 +16,15 @@ var firstname = flag.String("firstname", "", "first name")
 var lastname = flag.String("lastname", "", "last name")
 var order = flag.String("order", "", "order")
 
-type Response struct {
+type Order struct {
 	Firstname string `json:"firstname"`
 	Lastname string `json:"lastname"`
 	Order string `json:"order"`
+}
+
+type Response struct {
+	Statuscode int `json:"statuscode"`
+	Message string `json:"message"`
 }
 
 func main() {
@@ -31,31 +36,17 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Connecting to " + *host + ":" + *port)
-	done := make(chan string)
-	go sendData(conn, done)
-	// go getResponse(conn, done)
-	fmt.Println(<-done)
-	// fmt.Println(<-done)
+	sendDataNgetResponse(conn)
+	conn.Close()
 }
 
-func getResponse(conn net.Conn, done chan string) {
-	statuscode, err_r := ioutil.ReadAll(conn)
-	if err_r != nil {
-		fmt.Println("error reading: ", err_r)
-		panic(err_r)
-	}
-	i, _ := strconv.Atoi(string(statuscode))
-	fmt.Println("Status code from server is: ", i)
-	done <- "Read"
-}
-
-func sendData(conn net.Conn, done chan string) {
-	r := Response{
+func sendDataNgetResponse(conn net.Conn) {
+	order := Order{
 		Firstname: *firstname,
 		Lastname: *lastname,
 		Order: *order,
 	}
-	b, errs := json.Marshal(r)
+	b, errs := json.Marshal(order)
 	if errs != nil {
 		fmt.Println("Failed to serialize data")
 		return
@@ -66,6 +57,23 @@ func sendData(conn net.Conn, done chan string) {
 		return
 	}
 	fmt.Println("Succeeded to make a request to server with data: ", string(b))
-	done <- "Sent"
+
+	conn.(*net.TCPConn).CloseWrite()
+
+	message, err_r := ioutil.ReadAll(conn)
+
+	var r Response
+
+	json.Unmarshal(message, &r)
+
+	if err_r != nil {
+		fmt.Println("error reading: ", err_r)	
+	} else {
+		if r.Statuscode == 200 {
+			fmt.Println("Server succeeded to respond to the request with status code: ", strconv.Itoa(r.Statuscode))
+		} else {
+			fmt.Println("Server failed to respond to the request with status code: ", strconv.Itoa(r.Statuscode))
+		}
+	}
 }
 
--- src/lessons/tcpserver.go
+++ src/lessons/tcpserver.go
@@ -7,16 +7,20 @@ import (
 	"os"
 	"io/ioutil"
 	"encoding/json"
-	"strconv"
 )
 
 var host = flag.String("host", "", "host")
 var port = flag.String("port", "8800", "port")
 
-type Response struct {
+type Order struct {
 	Firstname string `json:"firstname"`
 	Lastname string `json:"lastname"`
 	Order string `json:"order"`
+}
+
+type Response struct {
+	Statuscode int `json:"statuscode"`
+	Message string `json:"message"`
 }
 
 func main() {
@@ -45,18 +49,28 @@ func handleRequest(conn net.Conn) {
 	defer conn.Close()
 	data, err := ioutil.ReadAll(conn) // return as bytes array
 	var statuscode int
+	var message string
 	if err != nil {
 		fmt.Println("reading err: ", err)
 		statuscode = 400
+		message = "Failed"
 	} else {
-		var r Response
-		json.Unmarshal(data, &r)
-		fmt.Println("Received: Firstname: " + r.Firstname, ", " + "Lastname: ", r.Lastname, ", " + "Order: ", r.Order)
+		var order Order
+		json.Unmarshal(data, &order)
+		fmt.Println("Received: Firstname: " + order.Firstname, ", " + "Lastname: ", order.Lastname, ", " + "Order: ", order.Order)
 		statuscode = 200
+		message = "Success"
 	}
-	_, err_r := conn.Write([]byte("Status code: " + strconv.Itoa(statuscode)))
+	response := Response{
+		Statuscode: statuscode,
+		Message: message,
+	}
+	m, err_m := json.Marshal(response)
+	if err_m != nil {
+		fmt.Println("Failed to serialize response")
+	}
+	_, err_r := conn.Write(m)
 	if err_r != nil {
 		fmt.Println("Failed to send status code to client")
 	}
-	fmt.Println("Sending status code: ", statuscode)
 }
