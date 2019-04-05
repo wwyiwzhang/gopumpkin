a5ff9b0b45ce1e234b1b04da0643956d7e756534
Weiwei Zhang <zww807865720@qq.com>
2019-04-05 13:08:16 -0700

    clean up worker (#9)

--- src/lessons/worker.go
+++ src/lessons/worker.go
@@ -26,36 +26,51 @@ func newWorker(wid int) *Worker {
 	}
 }
 
+type Pool struct {
+	workPool []*Worker
+}
+
+func newPool() *Pool {
+	return &Pool{
+		workPool: []*Worker{},
+	}
+}
+
+func (p *Pool) addWorker(wg *sync.WaitGroup, w *Worker) *Pool {
+	wg.Add(1)
+	p.workPool = append(p.workPool, w)
+	return p
+}
+
 func (w *Worker) Do(wg *sync.WaitGroup) {
+	defer wg.Done()
+loop:
 	for {
-		wrk, ok := <-w.wc
-		if !ok {
-			wg.Done()
-			break
+		if wrk, ok := <-w.wc; !ok {
+			break loop
+		} else {
+			fmt.Printf("Worker #%d is processing work: %v\n", w.id, wrk)
 		}
-		fmt.Printf("Worker: #%d is processing work: %v\n", w.id, wrk)
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
-	pool := []*Worker{}
-
+	pool := newPool()
 	for i := range []int{1, 2, 3, 4, 5} {
 		worker := newWorker(i)
-		pool = append(pool, worker)
-		wg.Add(1)
+		pool.addWorker(&wg, worker)
 		go worker.Do(&wg)
 	}
 
 	for j := range []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10} {
 		w := Work(j)
 		mod := j % 5
-		pool[mod].wc <- w
+		pool.workPool[mod].wc <- w
 	}
 
 	go func() {
-		for _, w := range pool {
+		for _, w := range pool.workPool {
 			fmt.Printf("closing channel for worker: #%d\n", w.id)
 			close(w.wc)
 		}
